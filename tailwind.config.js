dd6a08b3c14b5dab1750e9ba6208b946274567f8
enrihg <99612570+enrihg@users.noreply.github.com>
2024-07-02 12:30:07 -0300

    basic structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '50': '#f2f8fd',
+        '100': '#e5eff9',
+        '200': '#c5e0f2',
+        '300': '#9fccea',
+        '400': '#58a7d8',
+        '500': '#328cc5',
+        '600': '#2270a7',
+        '700': '#1d5987',
+        '800': '#1c4d70',
+        '900': '#1c415e',
+        '950': '#132a3e',
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
