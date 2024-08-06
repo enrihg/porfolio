523f64fb6abe62f3d201db7976d902f375cdaa3a
enrihg <99612570+enrihg@users.noreply.github.com>
2024-08-06 15:16:07 -0300

    CV links changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,8 @@ import Github from "../assets/icons/Github"
 import { useTranslation } from "react-i18next";
 
 function Header() {
-  const [t] = useTranslation("global");
+  const [t, i18n] = useTranslation("global");
+  const currentLanguage = i18n.language;
 
   return (
     <header id="header" className="sm:flex sm:justify-between sm:gap-12 pt-[70px] sm:pt-[78px] xl:pt-[86px] pb-16 px-[5%] sm:px-[10%] xl:px-[15%] 2xl:px-[20%] border-b border-gray-700">
@@ -14,7 +15,8 @@ function Header() {
         <p className="max-w-[400px]  mb-8 text-1000 dark:text-100">// {t("header.aboutMe")}</p>
         <div className="flex items-center gap-4 mb-12">
           <a href="#contact" className="flex justify-center items-center px-5 py-[10px] bg-600 hover:bg-700 active:bg-800 dark:bg-400 hover:dark:bg-500 active:dark:bg-600 text-50 dark:text-1000 font-semibold text-[14px] rounded-[100px]">{t("header.contact")}</a>
-          <a href="https://docs.google.com/document/d/1co1IJUgXaglv7IQ8Z0vVljNMQHtyJUHzA0hkECFy-_M/edit?usp=sharing" target="_blank" className="flex justify-center items-center w-10 h-10 border-2 border-600 hover:border-700 active:border-800 dark:border-400 dark:hover:border-500 dark:active:border-600 text-600 hover:text-700 active:text-800 dark:text-400 dark:hover:text-500 dark:active:text-600 font-semibold text-[18px] rounded-full">CV</a>
+          {currentLanguage === "en" ? <a href="https://drive.google.com/file/d/1lQbTlHR0Nvj5AFPwDakacixXH9NHyN93/view?usp=sharing" target="_blank" className="flex justify-center items-center w-10 h-10 border-2 border-600 hover:border-700 active:border-800 dark:border-400 dark:hover:border-500 dark:active:border-600 text-600 hover:text-700 active:text-800 dark:text-400 dark:hover:text-500 dark:active:text-600 font-semibold text-[18px] rounded-full">CV</a>
+            : <a href="https://drive.google.com/file/d/1N5kwvFzHi8WKPLjbhJHz6aBlLGeWwhPu/view?usp=sharing" target="_blank" className="flex justify-center items-center w-10 h-10 border-2 border-600 hover:border-700 active:border-800 dark:border-400 dark:hover:border-500 dark:active:border-600 text-600 hover:text-700 active:text-800 dark:text-400 dark:hover:text-500 dark:active:text-600 font-semibold text-[18px] rounded-full">CV</a>}
           <a href="https://www.linkedin.com/in/enrique-hauenstein-8093662a4" target="_blank" className="flex justify-center items-center w-10 h-10  border-2 border-600 hover:border-700 active:border-800 dark:border-400 dark:hover:border-500 dark:active:border-600 rounded-full fill-600 hover:fill-700 active:fill-800 dark:fill-400 dark:hover:fill-500 dark:active:fill-600 "><Linkedin className={"fill-inherit"} width={20} height={20} /></a>
           <a href="https://github.com/enrihg" target="_blank" className="flex justify-center items-center w-10 h-10 border-2 border-600 hover:border-700 active:border-800 dark:border-400 dark:hover:border-500 dark:active:border-600 rounded-full fill-600 hover:fill-700 active:fill-800 dark:fill-400 dark:hover:fill-500 dark:active:fill-600 "><Github className={"fill-inherit"} width={20} height={20} /></a>
         </div>
